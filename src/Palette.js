0b02368a779bc281503b7108da25eda4be12e4f8
v-poghosyan <vahrampoghosyan@live.com>
2021-06-07 17:33:56 -0700

    Created App component and added palettes

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return(
+      <div className="Palette">
+        {/* Navbar */}
+        <div className="Palette-colors">
+        
+        </div>
+        {/* Footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import App from './App'; /* The main App component to be rendered */
+import './index.css'; /* For setting global document styles: resetting unwanted white spaces in this case */
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<App />, document.getElementById('root'));
