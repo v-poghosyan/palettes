82b113070bed43e4759415ddd068089112d0813f
v-poghosyan <vahrampoghosyan@live.com>
2021-06-08 16:47:57 -0700

    Styled level slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import './Palette.css';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Palette.css'; /* Load this stylesheet before that if rc-slider to overwrite its styling */
 
 class Palette extends Component {
 
@@ -19,19 +19,28 @@ class Palette extends Component {
   }
 
   render () {
+
     const colors = this.props.palette.colors; /* Extracting the props */
     const sliderValue = this.state.sliderValue; /* Extracting the state */
     const colorBoxes = colors[sliderValue].map(col => <ColorBox background={col.hex} name={col.name}/>); /* An array of ColorBoxes */
 
     return ( 
       <div className="Palette">
-        <Slider 
-          defaultValue={this.state.sliderValue} 
-          min={100} 
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="Slider-wrapper">
+          <Slider 
+            defaultValue={this.state.sliderValue} 
+            min={100} 
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+            handleStyle={[{ /* Using this prop instead of simply selecting in CSS because of elusive :hover, :active, :focus, etc pseudo-classes */
+              backgroundColor: 'mediumseagreen', 
+              outline: 'none', 
+              border: 'none', 
+              marginTop: '-2px',
+              boxShadow: 'none'}]}
+          />
+        </div>
         {/* Navbar */}
         <div className="Pal-colors">{colorBoxes}</div>
         {/* Footer */}
