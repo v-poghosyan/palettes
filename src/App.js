23659e1cbd1e5e27afae4927af7cc08bbbc67fbb
v-poghosyan <vahrampoghosyan@live.com>
2021-06-09 15:07:43 -0700

    Hooked up palette routing to the paletter component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ import {generatePalette} from './colorHelpers';
 
 class App extends Component {
 
+  findPalette(id) { /* Return starter palette given id */
+    return seedPalettes.find((palette) => palette.id === id);
+  }
+
   render () {
     return (
       <Switch>
         <Route 
           exact 
           path="/" 
-          render={() => <h1>List goes here</h1>}></Route>
+          render={() => <h1>List goes here</h1>}
+        />
+        {/* Lines 26-27: Get starter palette based on the URL id, then generate new palette (with levels and formats). */} 
+        {/* Then pass it in as the palette prop to the component */}
         <Route 
           exact 
-          path="/palette/:id" 
-          render={() => <h1>Palette here</h1>}></Route>
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette 
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        /> 
       </Switch>
       
       // <div className="App">
