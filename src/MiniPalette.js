515f2d33d110d41a5976b455ecf75edda53a5db3
v-poghosyan <vahrampoghosyan@live.com>
2021-06-09 23:52:23 -0700

    Fixed fonts in Palette component

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -6,17 +6,20 @@ const styles = {
 
   MiniPalette: {
     backgroundColor: "white",
+    height: "20vh",
     padding: "0.5rem",
     position: "relative",
+    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
     overflow: "hidden",
-    border: "1px solid gray",
+    marginBottom: "1vh",
     "&:hover" : {
       cursor: "pointer"
     }
   },
 
   MP_colors: {
-    backgroundColor: "gray"
+    backgroundColor: "white",
+    height: "85%"
   },
 
   MP_title : {
@@ -27,11 +30,20 @@ const styles = {
     color: "black",
     paddingTop: "0.5rem",
     fontSize: "1rem",
-    position: "relative"
+    position: "relative",
   },
 
   MP_emoji: {
     marginLeft: "0.5rem"
+  },
+
+  MP_mini_box: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-4px"
   }
 }
 
@@ -39,12 +51,20 @@ const styles = {
 
 function MiniPalette(props) {
 
-  const {classes, paletteName, emoji } = props; /* Extracting props */
+  const {classes, paletteName, emoji, colors } = props; /* Extracting props */
 
   return (
     <div className={classes.MiniPalette}>
       <div className={classes.MP_colors}>
-
+        {/* Note 1: background style must be applied during the mapping (and not in JSS) as it's different for each mini color box */}
+        {/* Note 2: name is unique in seedPalettes and ensured to be unique at the time of adding a new palette, so we can use it as key */}
+        {colors.map(col => 
+          <div 
+            className={classes.MP_mini_box} 
+            style={{backgroundColor: col.color}}
+            key={col.name}
+          />
+        )}
       </div>
       <h5 className={classes.MP_title}>{paletteName}
         <span className={classes.MP_emoji}>{emoji}</span>
