df20af99e1200ead713d05f56fc2138db68aed61
v-poghosyan <vahrampoghosyan@live.com>
2021-06-10 19:14:07 -0700

    Added background luminosity-based text coloring

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {Link} from 'react-router-dom';
+import chroma from 'chroma-js'; /* Need this import to conditionally style text color based on the background color of a ColorBox */
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -22,21 +23,23 @@ class ColorBox extends Component {
 
     const {name, background, colorId, paletteId, moreLink} = this.props; /* Extracting the props */
     const copied = this.state.beingCopied; /* Extracting state */
+    const isDark = (chroma(background).luminance() <= 0.15); /* Boolean value for whether or not a color is dark */
+    const isLight = (chroma(background).luminance() >= 0.55); 
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div className="ColorBox" style={{background: background}}>
           {/* Next two divs are hidden overlays that appears only when ColorBox is being copied */}
           <div className={`CB-overlay ${copied ? 'show' : '' }`} style={{background: background}} />
-          <div className={`CB-copy-msg ${copied ? 'show' : '' }`}>
+          <div className={`CB-copy-msg ${copied ? 'show' : '' } ${isLight ? 'Dark-text' : ''}`}>
             <h1>Copied!</h1>
             <p>{background}</p>
           </div>
           <div className="CB-copy-container">
-            <div className="CB-box-name">
+            <div className={`CB-box-name ${isDark ? 'Light-text' : ''}`}>
               <span>{name}</span>
             </div>
-            <button className="CB-copy-button">Copy</button>
+            <button className={`CB-copy-button ${isLight ? 'Dark-text' : ''}`}>Copy</button>
             {/* Note 1: If ColorBox is being renderd from Palette, show 'see more' link. If being rendered from SingleColorPalette, don't */}
             {/* Note 2: stopPropagation() stops click event from propagating to parent CoptToClipboard, preventing copying */}
             {moreLink ? 
@@ -44,7 +47,7 @@ class ColorBox extends Component {
                 to={`/palette/${paletteId}/${colorId}`} 
                 onClick={(evt) => evt.stopPropagation()}
               > 
-                <span className="CB-see-more">More</span>
+                <span className={`CB-see-more ${isLight ? 'Dark-text' : ''}`}>More</span>
               </Link> : "" }
           </div>
         </div>
