9136f4a7e5ce18c5eb179e69c220c9e66f38bbf7
v-poghosyan <vahrampoghosyan@live.com>
2021-06-11 16:06:56 -0700

    Fixed the back button in SingleColorPalette by refactoring it to use withStyles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,6 +3,51 @@ import ColorBox from './ColorBox'; /* We will style ColorBox slightly differentl
 import Navbar from './Navbar';
 import Footer from './Footer';
 import {Link} from 'react-router-dom';
+import {withStyles} from '@material-ui/styles';
+
+const styles = {
+
+  Go_back: {
+    width: "20%",
+    height: "50%", /* Checks if ColorBox is being rendered by Palette or SingleColorPalette */
+    margin: "0 auto",
+    /* Inline-block elements accept both width and height properties */ 
+    display: "inline-block",
+    /* Positioning relatively means positioning relative to the element's normal (flow) position - Enables top/right/bottom/left properies */
+    position: "relative",
+    cursor: "pointer",
+    /* When styling CB-see-more, the boxes get spaced out - this line fixes that */
+    marginBottom: "-4px",
+    backgroundColor: "#556e7d",
+  },
+  
+  Go_back_button: {
+    width: "100px",
+    height: "30px",
+    /* Positioning absolutely in a positioned parent means positioning relative to that parent */
+    position: "absolute",
+    display: "inline-block",
+    /* Centering relative to parent */
+    top: "50%",
+    left: "50%",
+    /* Offsetting the button's own width and height for proper centering*/
+    marginLeft: "-50px", /* 50px = width/2 */
+    marginTop: "-15px",  /* 15px = height/2 */
+    /* Other styling */
+    fontFamily: "'Montserrat', sans-serif",
+    border: "none",
+    outline: "none",
+    background: "rgba(255,255,255,0.3)",
+    fontSize: "1rem", /* Units of 'rem' are referenced from 'root font size.' Unlike 'em' units, these don't get smalled with each nested element */
+    lineHeight: "30px", /* Same line height as the element itself: centers text vertically */
+    color: "white",
+    textTransform: "uppercase",
+    cursor: "pointer",
+    opacity: "1",
+    textAlign: "center"
+  }
+
+}
 
 class SingleColorPalette extends Component {
 
@@ -28,7 +73,7 @@ class SingleColorPalette extends Component {
 
   render() {
 
-    const {palette, colorId} = this.props; /* Extracting props */
+    const {classes, palette, colorId} = this.props; /* Extracting props */
     const {format} = this.state /* Extracting state */
     const shades = this.getShadesOfColor(palette, colorId);
     console.log(shades);
@@ -49,9 +94,9 @@ class SingleColorPalette extends Component {
               moreLink={false} 
             />
           )}
-          <div className="Go-back ColorBox"> {/* Back button has almost the same styling as a ColorBox */}
+          <div className={classes.Go_back}> {/* Back button has almost the same styling as a ColorBox */}
             <Link
-              className="Go-back-button CB-copy-button"
+              className={classes.Go_back_button}
               to={`/palette/${palette.id}`} 
             >Go back
             </Link>
@@ -64,4 +109,4 @@ class SingleColorPalette extends Component {
 
 }
 
-export default SingleColorPalette;
+export default withStyles(styles)(SingleColorPalette);
